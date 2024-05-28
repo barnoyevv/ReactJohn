53f5dd39ffd4609e36b60110319e279ebec59db9
Shohjahon <shohjahon@MacBook-Air-Shohjahon.local>
2024-05-28 15:36:06 +0500

    first commit

--- src/components/blog-page/blog-page.jsx
+++ src/components/blog-page/blog-page.jsx
@@ -1,11 +1,9 @@
-import NavBlog from "./nav/nav"
 import MainBlog from "./main/main"
-import FooterBlog from "./footer/footer"
 
 const BlogPage = () => {
 	return (
 		<div>
-			<h1>Blog</h1>
+			<MainBlog/>
 		</div>
 	)
 }
--- src/components/main-page/footer/Footer.jsx
+++ src/components/main-page/footer/Footer.jsx
@@ -4,12 +4,12 @@ const Footer = () => {
 	return (
 		<div>
 			<div className="icon">
-						<span><i className="fa-brands fa-square-facebook"></i></span>
-						<span><i className="fa-brands fa-instagram"></i></span>
-						<span><i className="fa-brands fa-twitter"></i></span>
-						<span><i className="fa-brands fa-linkedin"></i></span>
-					</div>
-					<p>Copyright ©2020 All rights reserved </p>
+				<span><i className="fa-brands fa-square-facebook"></i></span>
+				<span><i className="fa-brands fa-instagram"></i></span>
+				<span><i className="fa-brands fa-twitter"></i></span>
+				<span><i className="fa-brands fa-linkedin"></i></span>
+			</div>
+			<p>Copyright ©2020 All rights reserved </p>
 		</div>
 	)
 }
